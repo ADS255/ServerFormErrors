c5f97f3aab17df73a3275732a5804fd905c8b927
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2025-02-15 22:10:13 +0000

    basic error message below input displaying.

--- ServerFormErrors.js
+++ ServerFormErrors.js
@@ -1,4 +1,8 @@
 class ServerFormErrors {
+  static defaultConfig = {};
+
+  static initForm(formSelector) {}
+
   static handleErrors(formSelector, errors) {
     const form = document.querySelector(formSelector);
     if (!form) return;
@@ -7,10 +11,18 @@ class ServerFormErrors {
       .querySelectorAll(".sfe-error")
       .forEach((el) => el.classList.remove("sfe-error"));
 
+    form.querySelectorAll(".sfe-error-message").forEach((el) => el.remove());
+
     Object.entries(errors).forEach(([field, message]) => {
       let input = form.querySelector(`[name="${field}"]`);
       if (input) {
         input.classList.add("sfe-error");
+
+        let span = document.createElement("span");
+        span.classList.add("sfe-error-message");
+        span.textContent = message;
+
+        input.parentNode.insertBefore(span, input.nextSibling);
       }
     });
   }
